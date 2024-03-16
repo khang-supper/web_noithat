479a9b24e6e3bdd1546e885d015a30a8d41f2bc1
Nguyễn vi khang <127206782+khang-supper@users.noreply.github.com>
2024-03-16 20:47:13 +0700

    Category
    Category

--- projectSem4/src/main/java/project/spring/model/Categorie.java
+++ projectSem4/src/main/java/project/spring/model/Categorie.java
@@ -1,58 +1,46 @@
 package project.spring.model;
 
-public class Category {
+public class Categorie {
 	private int id;
-	private String name; // Tên loại
-	private String image; // Đường dẫn ảnh
-	private boolean isDelete; // Trạng thái xóa
-
-	public static String ID = "id";
-	public static String NAME = "name";
-	public static String IMAGE = "image";
-	public static String ISDELETE = "isDelete";
-
-	public Category() {
+    private String name;  // Tên loại
+    private String image;  // Đường dẫn ảnh
+    private boolean isDelete;  // Trạng thái xóa
+    
+    
+	public Categorie() {
 		super();
 	}
-
-	public Category(int id, String name, String image, boolean isDelete) {
+	public Categorie(int id, String name, String image, boolean isDelete) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.image = image;
 		this.isDelete = isDelete;
 	}
-
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getImage() {
 		return image;
 	}
-
 	public void setImage(String image) {
 		this.image = image;
 	}
-
-	public boolean getIsDelete() {
+	public boolean isDelete() {
 		return isDelete;
 	}
-
-	public void setIsDelete(boolean isDelete) {
+	public void setDelete(boolean isDelete) {
 		this.isDelete = isDelete;
 	}
-
+    
 }
+
